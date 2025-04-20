16f3ddefd853810f32f8cd990537688a402b75ec
PutraSumimasen <putraadnyana17rpl@gmail.com>
2025-04-20 11:19:00 +0700

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,36 @@
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    <div class="container">
+        <a class="navbar-brand" href="#page-top">
+            <img src="assets/img/Info Pelalian (2).svg" alt="Logo Pelalian" style="height: 80px !important; width: auto;" />
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
+            aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            Menu
+            <i class="fas fa-bars ms-1"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+                <li class="nav-item"><a class="nav-link" href="#services">Destinasi</a></li>
+                <li class="nav-item"><a class="nav-link" href="#portfolio">Anggota</a></li>
+                <li class="nav-item"><a class="nav-link" href="#about">Peralatan</a></li>
+                <li class="nav-item"><a class="nav-link" href="#team">Team</a></li>
+                <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+
+<!-- CSS (bisa taruh di style.css atau di dalam <style> di <head>) -->
+<style>
+    .logo-navbar {
+        height: 900px; /* Gedein sesuai kebutuhan, misal 80px juga bisa */
+    }
+
+    @media (max-width: 991px) {
+        .logo-navbar {
+            height: 50px; /* Logo lebih kecil di layar kecil kalau mau */
+        }
+    }
+</style>
